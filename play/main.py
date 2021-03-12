0e2708bc134234a3ac051963dc6d9f239b495c5a
Chishun Kwong <chishunkwong@yahoo.com>
2021-03-12 10:28:45 -0500

    solution to increasing triplet

--- play/main.py
+++ play/main.py
@@ -3,7 +3,9 @@ from solution import Solution
 
 def main():
     sol = Solution()
-    print(sol.increasingTriplet([1, 2, 3]))
+    print(sol.increasingTriplet([2, 1, 5, 0, 4, 6]))
+    print(sol.increasingTriplet([1, 5, 0, 4, 1, 3]))
+    print(sol.increasingTriplet([4, 5, 3, 4, 1, 2]))
 
 
 if __name__ == '__main__':
--- play/solution.py
+++ play/solution.py
@@ -3,4 +3,18 @@ from typing import List
 
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        return True
+        # as we walk from left to right, track these numbers:
+        # the smallest number in the array
+        minimum: int = None
+        # the smallest number in the array that has a number to the left that is smaller than it
+        has_smaller: int = None
+        for num in nums:
+            if minimum is None or num < minimum:
+                minimum = num
+            if minimum is not None and num > minimum:
+                # num is bigger than at least one number
+                if has_smaller is None or has_smaller > num:
+                    has_smaller = num
+            if has_smaller is not None and num > has_smaller:
+                return True
+        return False
